6f04a1a8a68cee3a052956cae6d35730dffe267f
Tryston Hoyes <trystonhoyes@gmail.com>
2019-06-08 13:33:42 -0400

    Update teiji_bot_code_for_bluetooth_control_V1.ino
    added code to control direction of front wheels

--- teiji_bot_code_for_bluetooth_control_V1.ino
+++ teiji_bot_code_for_bluetooth_control_V1.ino
@@ -1,15 +1,16 @@
 char t;
  
 void setup() {
-    pinMode(6, OUTPUT);
+    
 pinMode(13,OUTPUT);   //in1 neg motors forward
 pinMode(12,OUTPUT);   //in2 pos motors reverse
 pinMode(11,OUTPUT);   //right motors forward
 pinMode(10,OUTPUT);   //right motors reverse
 pinMode(6, OUTPUT);
+pinMode(3, OUTPUT);
 
 Serial.begin(9600);
- 
+
 }
  
 void loop() {
@@ -22,6 +23,7 @@ if(t == 'F'){            //move forward(all motors rotate in forward direction)
  digitalWrite(12,LOW);
   digitalWrite(13,HIGH);
   analogWrite(6, 255);
+analogWrite(3, 85);
 }
  
 else if(t == 'B'){      //move reverse (all motors rotate in reverse direction)
@@ -34,10 +36,15 @@ else if(t == 'B'){      //move reverse (all motors rotate in reverse direction)
  
 else if(t == 'L'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
   digitalWrite(11,HIGH);
+	analogWrite(3, 64);
+// range for left motor is 59-80
+
 }
  
 else if(t == 'R'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
   digitalWrite(13,HIGH);
+analogWrite(3, 95);
+// range for left motor is 90-110
 }
 
 else if(t == 'W'){    //turn led on or off)
